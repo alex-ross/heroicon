b0f18bc60f879b15e4bc596fd3826ad1c775f766
Alexander Ross <ross@standout.se>
2024-10-04 17:19:45 +0200

    Update dummy app
    - Resolve conflicts in gem versions
    - Use helper to loop through icons instead of require the developer to add them whenever they change.
    - Remove unnessesary usage of asset pipeline
    - Remove files including only comments

--- test/dummy/app/helpers/heroicon_helper.rb
+++ test/dummy/app/helpers/heroicon_helper.rb
@@ -2,4 +2,12 @@
 
 module HeroiconHelper
   include Heroicon::Engine.helpers
+
+  def names_for_variant(variant)
+    gem_path = Gem.loaded_specs['heroicon'].full_gem_path
+    variant_path = "#{gem_path}/app/assets/images/heroicon/#{variant}/"
+    Dir["#{variant_path}/*.svg"].map do |path|
+      File.basename(path, '.svg')
+    end
+  end
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,18 +1,27 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "heroicon"
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}".to_f
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
 
-    if Rails::VERSION::MAJOR == 5
-      config.active_record.sqlite3.represent_boolean_as_integer = true
-    end
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
